24913be2aa5e0ad2632dfa62299ac398f2719ae9
Seajay <cjmcerezo2021@plm.edu.ph>
2024-10-16 00:29:20 +0800

    update

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.2/dist/cdn.min.js"></script>
+
+  <title>EduBridge</title>
+</head>
+
+
+<body class=" justify-center items-center h-screen bg-light">
+  <div class="flex flex-col text-center">
+    <h1 class=" text-primary mb-4">I LOVE CJ ! I AM A FAN hehe</h1>
+    <span class = "text-warning" >This is the homepage</span>
+  </div>
+</body>
+</html>
