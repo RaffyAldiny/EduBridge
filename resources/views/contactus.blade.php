d72de7ffe1b827088f49dbc0637ef195cc7b0949
Yugi <84443157+RaffyAldiny@users.noreply.github.com>
2024-10-21 01:31:47 +0800

    homepage

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -8,6 +8,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.2/dist/cdn.min.js"></script>
 
   <title>EduBridge</title>
