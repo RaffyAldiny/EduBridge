cc65de229196288d3b916622e6de77c7027c290c
Yugi <84443157+RaffyAldiny@users.noreply.github.com>
2024-10-21 01:29:53 +0800

    homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,6 +7,7 @@
   @livewireStyles
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
   <title>EduBridge</title>
