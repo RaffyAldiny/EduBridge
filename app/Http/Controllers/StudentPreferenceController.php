90ea730ee5b017f21909892efeec4c6c1938a3d0
Yugi <84443157+RaffyAldiny@users.noreply.github.com>
2024-10-21 19:12:58 +0800

    fix titles

--- app/Http/Controllers/StudentPreferenceController.php
+++ app/Http/Controllers/StudentPreferenceController.php
@@ -8,6 +8,12 @@ class StudentPreferenceController extends Controller
 {
     public function showProfessionList($profession)
     {
+        // Manually map professions that don't follow a simple rule
+        $manualTitles = [
+            'dataanalyst' => 'Data Analyst',
+            'peerreviewer' => 'Peer Reviewer'
+        ];
+    
         // Same list of professionals for all professions
         $professionals = [
             ['name' => 'John Cris A. Demafeliz', 'profile_picture' => 'https://via.placeholder.com/40', 'is_active' => true, 'last_active' => 'Active'],
@@ -15,7 +21,18 @@ class StudentPreferenceController extends Controller
             ['name' => 'Anna Clark', 'profile_picture' => 'https://via.placeholder.com/40', 'is_active' => true, 'last_active' => 'Active'],
             ['name' => 'Bob Harris', 'profile_picture' => 'https://via.placeholder.com/40', 'is_active' => false, 'last_active' => '5 hours ago']
         ];
-
-        return view('professionlist', ['profession' => $profession, 'professionals' => $professionals]);
+    
+        // If the profession exists in manualTitles, use that title
+        if (array_key_exists($profession, $manualTitles)) {
+            $professionTitle = $manualTitles[$profession];
+        } else {
+            // Otherwise, automatically format the profession slug
+            $professionTitle = ucwords(str_replace(['-', '_'], ' ', $profession));
+        }
+    
+        return view('professionlist', [
+            'profession' => $professionTitle, // Dynamic conversion or manual mapping
+            'professionals' => $professionals
+        ]);
     }
 }
