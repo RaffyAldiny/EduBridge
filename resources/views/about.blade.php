27abffac2f19ee8c0bc90f1e923b145dd89f24d0
Seajay <cjmcerezo2021@plm.edu.ph>
2024-10-23 03:30:12 +0800

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     @livewireStyles
-
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -45,6 +44,7 @@
         /* Set position: relative on the first section */
         section {
             position: relative;
+            z-index: 10;
             height: auto;
             display: flex;
             align-items: center;
