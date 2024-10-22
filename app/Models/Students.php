af0f83d7f2584b03eb1cd1ddd01e5bfdc52ea6e9
Seajay <cjmcerezo2021@plm.edu.ph>
2024-10-23 01:22:41 +0800

    sign up done

---
+++ app/Models/Students.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Students extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = ['name', 'email', 'password', 'school_id_path'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+}
