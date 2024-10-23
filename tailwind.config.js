defffe7f46d91b0a08aa1763ec701d55e8ffd9c3
Yugi <84443157+RaffyAldiny@users.noreply.github.com>
2024-10-23 20:27:33 +0800

    safelisting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    // Safelisting whitehover to prevent it from being purged
+    {
+      safelist: ['bg-whitehover', 'hover:bg-whitehover','text-whitehover'],
+    },
   ],
   theme: {
     extend: {
       colors: {
         'maincolor': '#c4eaf7',
         'hover': '#0b263b',
-        'whitehover' : '#FEFEFE',
+        'whitehover': '#FEFEFE',
       },
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
